c5ce083f6233bc781c767e68468b0480c9867e26
Matias Yacob <matiasyacob@hotmail.com>
2026-01-08 11:44:40 -0300

    creada la funcion para traer palabras desde la API de la RAE

---
+++ wordle.py
@@ -0,0 +1,32 @@
+import requests
+
+class RAEApi:
+    URL = "https://rae-api.com/api/random"
+
+    #==== Función para obtener una palabra aleatoria ====#
+    def get_random_word(self, length=None, min_length=None, max_length=None):
+        params = {}
+
+        if length is not None:
+            params["min_length"] = length
+            params["max_length"] = length
+        else:
+            if min_length is not None:
+                params["min_length"] = min_length
+            if max_length is not None:
+                params["max_length"] = max_length
+
+        response = requests.get(self.URL, params=params)
+        data = response.json()
+        return self._quitar_acentos(data["data"]["word"])
+
+    #==== Función para quitar acentos ====#
+    def _quitar_acentos(self, texto):
+        return texto.translate(
+            str.maketrans("áéíóúÁÉÍÓÚ", "aeiouAEIOU")
+        )
+
+rae = RAEApi()
+
+print(rae.get_random_word(length=7))   # exactamente 7 letras
+
